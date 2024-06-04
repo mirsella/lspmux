EliteTK/ra-multiplex

support unix socket addresses in listen and connect

src/config.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeSet;
 use std::fs;
 use std::net::{IpAddr, Ipv4Addr};
+use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use directories::ProjectDirs;
@@ -21,12 +22,12 @@ mod default {
         10
     }
 
-    pub fn listen() -> (IpAddr, u16) {
+    pub fn listen() -> ListenAddr {
         // localhost & some random unprivileged port
-        (IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 27_631)
+        ListenAddr::Tcp(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 27_631)
     }
 
-    pub fn connect() -> (IpAddr, u16) {
+    pub fn connect() -> ListenAddr {
         listen()
     }
 
@@ -83,6 +84,13 @@ mod de {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ListenAddr {
+    Tcp(IpAddr, u16),
+    Unix(PathBuf),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
     #[serde(default = "default::instance_timeout")]
@@ -94,10 +102,10 @@ pub struct Config {
     pub gc_interval: u32,
 
     #[serde(default = "default::listen")]
-    pub listen: (IpAddr, u16),
+    pub listen: ListenAddr,
 
     #[serde(default = "default::connect")]
-    pub connect: (IpAddr, u16),
+    pub connect: ListenAddr,
 
     #[serde(default = "default::log_filters")]
     pub log_filters: String,

src/ext.rs
@@ -4,7 +4,7 @@ use anyhow::{bail, Context, Result};
 use serde::de::{DeserializeOwned, IgnoredAny};
 use tokio::io::BufReader;
 
-use crate::config::Config;
+use crate::config::{Config, ListenAddr};
 use crate::lsp::ext::{self, LspMuxOptions, StatusResponse};
 use crate::lsp::jsonrpc::{Message, Request, RequestId, Version};
 use crate::lsp::transport::{LspReader, LspWriter};
@@ -15,10 +15,12 @@ pub async fn ext_request<T>(config: &Config, method: ext::Request) -> Result<T>
 where
     T: DeserializeOwned,
 {
-    let (reader, writer) = Stream::connect_tcp(config.connect)
-        .await
-        .context("connect")?
-        .into_split();
+    let (reader, writer) = match config.connect {
+        ListenAddr::Tcp(ip_addr, port) => Stream::connect_tcp((ip_addr, port)).await,
+        ListenAddr::Unix(ref path) => Stream::connect_unix(path).await,
+    }
+    .context("connect")?
+    .into_split();
     let mut writer = LspWriter::new(writer, "lspmux");
     let mut reader = LspReader::new(BufReader::new(reader), "lspmux");
 

src/proxy.rs
@@ -4,7 +4,7 @@ use std::env;
 use anyhow::{bail, Context as _, Result};
 use tokio::io::{self, BufStream};
 
-use crate::config::Config;
+use crate::config::{Config, ListenAddr};
 use crate::lsp::ext::{LspMuxOptions, Request};
 use crate::lsp::jsonrpc::Message;
 use crate::lsp::transport::{LspReader, LspWriter};
@@ -23,9 +23,11 @@ pub async fn run(config: &Config, server: String, args: Vec<String>) -> Result<(
         }
     }
 
-    let mut stream = Stream::connect_tcp(config.connect)
-        .await
-        .context("connect")?;
+    let mut stream = match config.connect {
+        ListenAddr::Tcp(ip_addr, port) => Stream::connect_tcp((ip_addr, port)).await,
+        ListenAddr::Unix(ref path) => Stream::connect_unix(path).await,
+    }
+    .context("connect")?;
     let mut stdio = BufStream::new(io::join(io::stdin(), io::stdout()));
 
     // Wait for the client to send `initialize` request.

src/server.rs
@@ -5,7 +5,7 @@ use tokio::task;
 use tracing::{error, info, info_span, warn, Instrument};
 
 use crate::client;
-use crate::config::Config;
+use crate::config::{Config, ListenAddr};
 use crate::instance::InstanceMap;
 use crate::socketwrapper::Listener;
 
@@ -13,9 +13,11 @@ pub async fn run(config: &Config) -> Result<()> {
     let instance_map = InstanceMap::new(config).await;
     let next_client_id = AtomicUsize::new(0);
 
-    let listener = Listener::bind_tcp(config.listen.into())
-        .await
-        .context("listen")?;
+    let listener = match config.listen {
+        ListenAddr::Tcp(ip_addr, port) => Listener::bind_tcp((ip_addr, port).into()).await,
+        ListenAddr::Unix(ref path) => Listener::bind_unix(path),
+    }
+    .context("listen")?;
     loop {
         match listener.accept().await {
             Ok((socket, _addr)) => {

mirsella/lspmux

src/config.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeSet;
 use std::fs;
 use std::net::{IpAddr, Ipv4Addr};
+#[cfg(target_family = "unix")]
+use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use directories::ProjectDirs;
@@ -21,12 +23,12 @@ mod default {
         10
     }
 
-    pub fn listen() -> (IpAddr, u16) {
+    pub fn listen() -> Address {
         // localhost & some random unprivileged port
-        (IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 27_631)
+        Address::Tcp(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 27_631)
     }
 
-    pub fn connect() -> (IpAddr, u16) {
+    pub fn connect() -> Address {
         listen()
     }
 
@@ -83,6 +85,14 @@ mod de {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Address {
+    Tcp(IpAddr, u16),
+    #[cfg(target_family = "unix")]
+    Unix(PathBuf),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
     #[serde(default = "default::instance_timeout")]
@@ -94,10 +104,10 @@ pub struct Config {
     pub gc_interval: u32,
 
     #[serde(default = "default::listen")]
-    pub listen: (IpAddr, u16),
+    pub listen: Address,
 
     #[serde(default = "default::connect")]
-    pub connect: (IpAddr, u16),
+    pub connect: Address,
 
     #[serde(default = "default::log_filters")]
     pub log_filters: String,

src/ext.rs
@@ -4,7 +4,7 @@ use anyhow::{bail, Context, Result};
 use serde::de::{DeserializeOwned, IgnoredAny};
 use tokio::io::BufReader;
 
-use crate::config::Config;
+use crate::config::{Address, Config};
 use crate::lsp::ext::{self, LspMuxOptions, StatusResponse};
 use crate::lsp::jsonrpc::{Message, Request, RequestId, Version};
 use crate::lsp::transport::{LspReader, LspWriter};
@@ -15,10 +15,13 @@ pub async fn ext_request<T>(config: &Config, method: ext::Request) -> Result<T>
 where
     T: DeserializeOwned,
 {
-    let (reader, writer) = Stream::connect_tcp(config.connect)
-        .await
-        .context("connect")?
-        .into_split();
+    let (reader, writer) = match config.connect {
+        Address::Tcp(ip_addr, port) => Stream::connect_tcp((ip_addr, port)).await,
+        #[cfg(target_family = "unix")]
+        Address::Unix(ref path) => Stream::connect_unix(path).await,
+    }
+    .context("connect")?
+    .into_split();
     let mut writer = LspWriter::new(writer, "lspmux");
     let mut reader = LspReader::new(BufReader::new(reader), "lspmux");
 

src/proxy.rs
@@ -4,7 +4,7 @@ use std::env;
 use anyhow::{bail, Context as _, Result};
 use tokio::io::{self, BufStream};
 
-use crate::config::Config;
+use crate::config::{Address, Config};
 use crate::lsp::ext::{LspMuxOptions, Request};
 use crate::lsp::jsonrpc::Message;
 use crate::lsp::transport::{LspReader, LspWriter};
@@ -23,9 +23,12 @@ pub async fn run(config: &Config, server: String, args: Vec<String>) -> Result<(
         }
     }
 
-    let mut stream = Stream::connect_tcp(config.connect)
-        .await
-        .context("connect")?;
+    let mut stream = match config.connect {
+        Address::Tcp(ip_addr, port) => Stream::connect_tcp((ip_addr, port)).await,
+        #[cfg(target_family = "unix")]
+        Address::Unix(ref path) => Stream::connect_unix(path).await,
+    }
+    .context("connect")?;
     let mut stdio = BufStream::new(io::join(io::stdin(), io::stdout()));
 
     // Wait for the client to send `initialize` request.

src/server.rs
@@ -5,7 +5,7 @@ use tokio::task;
 use tracing::{error, info, info_span, warn, Instrument};
 
 use crate::client;
-use crate::config::Config;
+use crate::config::{Address, Config};
 use crate::instance::InstanceMap;
 use crate::socketwrapper::Listener;
 
@@ -13,9 +13,12 @@ pub async fn run(config: &Config) -> Result<()> {
     let instance_map = InstanceMap::new(config).await;
     let next_client_id = AtomicUsize::new(0);
 
-    let listener = Listener::bind_tcp(config.listen.into())
-        .await
-        .context("listen")?;
+    let listener = match config.listen {
+        Address::Tcp(ip_addr, port) => Listener::bind_tcp((ip_addr, port).into()).await,
+        #[cfg(target_family = "unix")]
+        Address::Unix(ref path) => Listener::bind_unix(path),
+    }
+    .context("listen")?;
     loop {
         match listener.accept().await {
             Ok((socket, _addr)) => {